31afa44ee0bcbb27918cda3f79080ad2946ac6d6
Adam Raider <adamjraider@gmail.com>
2016-11-24 10:53:47 -0500

    adds content and mp3s

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template lang="pug">
-#app
-  h1 Raider
-  h2 Electronic Music
+#app.container
+  h1 Raider, music for people with bad taste
+  h2 Download my crappy music
+  ul
+    template(v-for="song in songs")
+      li
+        a(v-bind:href='"mp3/" + song.filename' target='_blank') {{ song.title }}
+  pre Right click -> save link as...
+  h2 Follow me on the internet
   ul
     li
-      a(href='https://vuejs.org', target='_blank') Core Docs
+      a(href='http://twitter.com/ixraider/' target='_blank') Twitter
     li
-      a(href='https://forum.vuejs.org', target='_blank') Forum
+      a(href='http://soundcloud.com/raider/' target='_blank') Soundcloud
     li
-      a(href='https://gitter.im/vuejs/vue', target='_blank') Gitter Chat
+      a(href='http://youtube.com/ixraider' target='_blank') Youtube
     li
-      a(href='https://twitter.com/vuejs', target='_blank') Twitter
-  h2 Ecosystem
-  ul
+      a(href='http://instagram.com/ixraider' target='_blank') Instagram
     li
-      a(href='http://router.vuejs.org/', target='_blank') vue-router
-    li
-      a(href='http://vuex.vuejs.org/', target='_blank') vuex
-    li
-      a(href='http://vue-loader.vuejs.org/', target='_blank') vue-loader
-    li
-      a(href='https://github.com/vuejs/awesome-vue', target='_blank') awesome-vue
+      a(href='http://facebook.com/officialraider' target='_blank') Faceboob
 </template>
 
 <script>
@@ -28,7 +26,24 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Raider'
+      msg: 'Raider',
+      songs: [
+        { title: "Rain (Raider Remix)", filename: "Astronaut - Rain (Raider Remix).mp3" },
+        { title: "Etho's Anthem VIP", filename: "Raider - Etho's Anthem VIP.mp3" },
+        { title: "Etho's Anthem", filename: "Raider - Etho's Anthem.mp3" },
+        { title: "Futuro", filename: "Raider - Futuro.mp3" },
+        { title: "Globe", filename: "Raider - Globe.mp3" },
+        { title: "Kaizen", filename: "Raider - Kaizen (Original Mix).mp3" },
+        { title: "Ordinance", filename: "Raider - Ordinance (Original Mix).mp3" },
+        { title: "Revolution (Remastered)", filename: "Raider - Revolution (Remastered).mp3" },
+        { title: "Somnium", filename: "Raider - Somnium.mp3" },
+        { title: "The Girl (Raider Remix)", filename: "Raider - The Girl - 01 Hellberg's The Girl feat. Cozi Zuehlsdorff (Raider Remix).mp3" },
+        { title: "Tremble (feat. Yoe Mase)", filename: "Raider - Tremble - 01 Tremble (feat. Yoe Mase).mp3" },
+        { title: "Vanguard", filename: "Raider - Vanguard.mp3" },
+        { title: "Finestorm", filename: "Raider & Lower Sixth - Finestorm.mp3" },
+        { title: "Illusion", filename: "Raider & Lower Sixth - Illusion.mp3" },
+        { title: "Colors (Raider Remix)", filename: "Tritonal & Paris Blohm - Colors (Raider Remix).mp3" },
+      ]
     }
   }
 }
@@ -39,9 +54,12 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif
   -webkit-font-smoothing: antialiased
   -moz-osx-font-smoothing: grayscale
-  text-align: center
   color: #2c3e50
   margin-top: 70px
+
+.container
+  max-width: 780px
+  margin: 0 auto
 
 h1, h2
   font-weight: normal
